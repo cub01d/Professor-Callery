08de9b4e7b92b75debd2d4b0df570dddffa284b4
Kenneth Chan <kc135576@aol.com>
2018-07-22 00:46:28 -0700

    Add option to load secrets from file or env

--- bot.js
+++ bot.js
@@ -3,26 +3,26 @@
 // const mySQL = require('mysql');
 
 const client = require('./src/client');
-//const config = require('./config/secrets.json');
-const config =
-{
-  "discord": {
-    "token": process.env.DISCORD_TOKEN,
-    "BOTID": process.env.DISCORD_BOTID
-  },
-  "mysql": {
-    "host": process.env.MYSQL_HOST,
-    "user": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_PASSWORD,
-    "database": process.env.MYSQL_DB
-  },
-  "webhook": {
-    "log": {
-	  "id": process.env.WEBHOOK_ID,
-	  "token": process.env.WEBHOOK_TOKEN
-    }
-  }
-};
+const config = (process.env.USE_SECRETS_FILE === "true") ?
+    require('./config/secrets.json') :
+    {
+      "discord": {
+        "token": process.env.DISCORD_TOKEN,
+        "BOTID": process.env.DISCORD_BOTID
+      },
+      "mysql": {
+        "host": process.env.MYSQL_HOST,
+        "user": process.env.MYSQL_USER,
+        "password": process.env.MYSQL_PASSWORD,
+        "database": process.env.MYSQL_DB
+      },
+      "webhook": {
+        "log": {
+          "id": process.env.WEBHOOK_ID,
+          "token": process.env.WEBHOOK_TOKEN
+        }
+      }
+    };
 
 const token = config.discord.token;
 /* const db = mySQL.createConnection(config.mysql);
