2caac043ecbf04f744d6363aacdb24a9811477ec
Kenneth Chan <kc135576@aol.com>
2018-07-23 00:27:13 -0700

    Secrets/env

--- controllers/switches.js
+++ controllers/switches.js
@@ -1,26 +1,26 @@
 const client = require('../src/client');
-//const config = require('../config/secrets.json');
 const logger = require('../logger');
+const config = (process.env.USE_SECRETS_FILE == "true") ?
+    require('../config/secrets.json') :
+    {
+      "discord": {
+        "token": process.env.DISCORD_TOKEN,
+        "BOTID": process.env.DISCORD_BOTID
+      },
+      "mysql": {
+        "host": process.env.MYSQL_HOST,
+        "user": process.env.MYSQL_USER,
+        "password": process.env.MYSQL_PASSWORD,
+        "database": process.env.MYSQL_DB
+      },
+      "webhook": {
+        "log": {
+          "id": process.env.WEBHOOK_ID,
+          "token": process.env.WEBHOOK_TOKEN
+        }
+      }
+    };
 const token = config.discord.token;
-const config =
-{
-  "discord": {
-    "token": process.env.DISCORD_TOKEN,
-    "BOTID": process.env.DISCORD_BOTID
-  },
-  "mysql": {
-    "host": process.env.MYSQL_HOST,
-    "user": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_PASSWORD,
-    "database": process.env.MYSQL_DB
-  },
-  "webhook": {
-    "log": {
-	  "id": process.env.WEBHOOK_ID,
-	  "token": process.env.WEBHOOK_TOKEN
-    }
-  }
-};
 
 function Off (req, res) {
     logger.info({ event: 'Turning bot off' });
